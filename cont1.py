5d44013d2fb29a7fb0fcf8b40a391f92c54e6cfe
Flávia Laís Ferreira de França <flavialais@MacBook-Air-de-Equipamentos.local>
2021-05-14 16:31:38 -0300

    primeiros passos Python e Github

--- cont1.py
+++ cont1.py
@@ -1,3 +1,21 @@
+from typing import Optional
+
+
+print('tentando aprender Python e Github')
+print('Oi, tudo bem?')
+#linguagem Python
+print(2+3)
+print(2-2)
+print(5*3)
+print(10/2)
+print(23/2)
+print(8**2)
+
+
+
+
+
+
 x = 3
 y = 3
 z = 4
